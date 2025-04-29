e0eee114058cb6b15e6823b14a8b459612d15a9f
ckz <18869978285@163.com>
2025-04-29 10:36:12 +0800

    fix:升级3.29.3

--- example/android/app/src/main/kotlin/com/example/install_apk_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/install_apk_plugin_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.install_apk_plugin_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
